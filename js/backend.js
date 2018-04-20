bb4edf2a9335e5b7a4bee85428629b87749883f1
Olga <fej-d@yandex.ru>
2018-04-20 11:56:31 +0300

    Взаимодествие с сервером

---
+++ js/backend.js
@@ -0,0 +1,49 @@
+'use strict';
+
+window.backend = (function () {
+  return {
+    load: function (onLoad, onError) {
+      var URL = 'https://js.dump.academy/code-and-magick/data';
+
+      var xhr = new XMLHttpRequest();
+      xhr.responseType = 'json';
+
+      xhr.addEventListener('load', function () {
+        if (xhr.status === 200) {
+          onLoad(xhr.response);
+        } else {
+          onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+        }
+      });
+
+      xhr.addEventListener('error', function () {
+        onError('Произошла ошибка соединения');
+      });
+
+      xhr.open('GET', URL);
+      xhr.send();
+    },
+
+    save: function (data, onLoad, onError) {
+      var URL = 'https://js.dump.academy/code-and-magick1';
+
+      var xhr = new XMLHttpRequest();
+      xhr.responseType = 'json';
+
+      xhr.addEventListener('load', function () {
+        if (xhr.status === 200) {
+          onLoad();
+        } else {
+          onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+        }
+      });
+
+      xhr.addEventListener('error', function () {
+        onError('Произошла ошибка соединения');
+      });
+
+      xhr.open('POST', URL);
+      xhr.send(data);
+    }
+  };
+})();
